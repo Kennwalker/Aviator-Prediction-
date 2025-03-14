c1f7a20c732550e9f39cf3c197c6433bcd38f6a4
Kennwalker <kennwalker222@gmail.com>
2025-03-14 06:17:28 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PredictionPage from "./components/PredictionPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aviator Prediction</h1>
+      <PredictionPage />
+    </div>
+  );
+}
+
+export default App;
